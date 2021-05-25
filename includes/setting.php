0228304439c06dc5352a3f45f395e558c65ffa86
Abhinav Dhiman <dhiman.ahnv@gmail.com>
2021-05-25 16:35:18 +0530

    Beautified PHP Code
    Internal JIRA Ticket: IMAGEKIT-67

--- includes/setting.php
+++ includes/setting.php
@@ -1,75 +1,73 @@
 <?php
+require_once (ABSPATH . 'wp-admin/includes/plugin-install.php');
 
-require_once( ABSPATH . 'wp-admin/includes/plugin-install.php' );
+function get_plugin_file($plugin_slug) {
+  require_once (ABSPATH . '/wp-admin/includes/plugin.php');
+  $plugins = get_plugins();
 
-function get_plugin_file( $plugin_slug ) {
-	 require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
-	 $plugins = get_plugins();
-
-	 foreach( $plugins as $plugin_file => $plugin_info ) {
-		 $slug = dirname( plugin_basename( $plugin_file ) );
-		 if($slug){
-			if ( $slug == $plugin_slug ) {
-			   return $plugin_file;
-			}
-		}
-	 }
-	 return null;
+  foreach ($plugins as $plugin_file => $plugin_info) {
+    $slug = dirname(plugin_basename($plugin_file));
+    if ($slug) {
+      if ($slug == $plugin_slug) {
+        return $plugin_file;
+      }
+    }
+  }
+  return null;
 }
 
-function check_file_extension( $filename ) {
-	if( substr( strrchr($filename, '.' ), 1 ) === 'php' ){
-		return true;
-	} else {
-		return false;
-	}
+function check_file_extension($filename) {
+  if (substr(strrchr($filename, '.') , 1) === 'php') {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
-
 
 function imagekit_render_imagekit_setting_page() {
-	global $imagekit_options;
-	
-	$plugin = "wp-lazy-loading";
-	
-	$api = plugins_api( 'plugin_information',
-		array(
-			'slug' => $plugin,
-			'fields' => array(
-				   'short_description' => true,
-					'sections' => false,
-					'requires' => false,
-					'downloaded' => true,
-					'last_updated' => false,
-					'added' => false,
-					'tags' => false,
-					'compatibility' => false,
-					'homepage' => false,
-					'donate_link' => false,
-					'icons' => true,
-					'banners' => true,
-			),
-		)
-	);
+  global $imagekit_options;
 
+  $plugin = "wp-lazy-loading";
 
-	$imagekit_options["cname"] = !empty($imagekit_options["cname"]) ? $imagekit_options["cname"] : "";
-	$imagekit_options["file_type"] = !empty($imagekit_options["file_type"]) ? $imagekit_options["file_type"] : "*.gif;*.png;*.jpg;*.jpeg;*.bmp;*.ico;*.webp";
-	$imagekit_options["custom_files"] = !empty($imagekit_options["custom_files"]) ? $imagekit_options["custom_files"] : "favicon.ico\ncustom-directory";
-	$imagekit_options["reject_files"] = !empty($imagekit_options["reject_files"]) ? $imagekit_options["reject_files"] : "wp-content/uploads/wpcf7_captcha/*\nwp-content/uploads/imagerotator.swf\ncustom-directory*.mp4";
+  $api = plugins_api('plugin_information', array(
+    'slug' => $plugin,
+    'fields' => array(
+      'short_description' => true,
+      'sections' => false,
+      'requires' => false,
+      'downloaded' => true,
+      'last_updated' => false,
+      'added' => false,
+      'tags' => false,
+      'compatibility' => false,
+      'homepage' => false,
+      'donate_link' => false,
+      'icons' => true,
+      'banners' => true,
+    ) ,
+  ));
 
-	if(empty($imagekit_options["imagekit_url_endpoint"])) {
-		if(empty($imagekit_options['imagekit_id']) && empty($imagekit_options['cname'])) {
-			$imagekit_options["imagekit_url_endpoint"] = "";
-		} else if(!empty($imagekit_options['cname'])) {
-			$imagekit_options["imagekit_url_endpoint"] = $imagekit_options['cname'];
-		} else if(!empty($imagekit_options['imagekit_id'])) {
-			$imagekit_options["imagekit_url_endpoint"] = "https://ik.imagekit.io/".$imagekit_options['imagekit_id'];
-		}
-	}
-	
-	ob_start();
+  $imagekit_options["cname"] = !empty($imagekit_options["cname"]) ? $imagekit_options["cname"] : "";
+  $imagekit_options["file_type"] = !empty($imagekit_options["file_type"]) ? $imagekit_options["file_type"] : "*.gif;*.png;*.jpg;*.jpeg;*.bmp;*.ico;*.webp";
+  $imagekit_options["custom_files"] = !empty($imagekit_options["custom_files"]) ? $imagekit_options["custom_files"] : "favicon.ico\ncustom-directory";
+  $imagekit_options["reject_files"] = !empty($imagekit_options["reject_files"]) ? $imagekit_options["reject_files"] : "wp-content/uploads/wpcf7_captcha/*\nwp-content/uploads/imagerotator.swf\ncustom-directory*.mp4";
 
-	wp_enqueue_style('xyz', plugins_url('imagekit') . '/includes/main.css');
+  if (empty($imagekit_options["imagekit_url_endpoint"])) {
+    if (empty($imagekit_options['imagekit_id']) && empty($imagekit_options['cname'])) {
+      $imagekit_options["imagekit_url_endpoint"] = "";
+    }
+    else if (!empty($imagekit_options['cname'])) {
+      $imagekit_options["imagekit_url_endpoint"] = $imagekit_options['cname'];
+    }
+    else if (!empty($imagekit_options['imagekit_id'])) {
+      $imagekit_options["imagekit_url_endpoint"] = "https://ik.imagekit.io/" . $imagekit_options['imagekit_id'];
+    }
+  }
+
+  ob_start();
+
+  wp_enqueue_style('xyz', plugins_url('imagekit') . '/includes/main.css');
 ?>
 <div>
    <div id="ik-plugin-container">
@@ -105,7 +103,7 @@ function imagekit_render_imagekit_setting_page() {
 										 type="text" 
 										 class="dops-text-input" 
                                          name="imagekit_settings[imagekit_url_endpoint]" 
-                                         value="<?php echo isset($imagekit_options['imagekit_url_endpoint']) ? $imagekit_options['imagekit_url_endpoint'] : '';?>" />
+                                         value="<?php echo isset($imagekit_options['imagekit_url_endpoint']) ? $imagekit_options['imagekit_url_endpoint'] : ''; ?>" />
                                 </label>
                                 <span class="ik-form-setting-explanation">
 									Copy paste the ImageKit URL endpoint (or CNAME) from ImageKit <a href="https://imagekit.io/dashboard#integration" target="_blank">dashboard</a>. 
@@ -169,21 +167,26 @@ function imagekit_render_imagekit_setting_page() {
 							<fieldset class="ik-form-fieldset">
 								<label class="ik-form-label"><span class="ik-form-label-wide"><?php _e('Lazy Load Images', 'imagekit_domain');; ?></span></label>
 								<p>Lazy loading images will improve your site’s speed and create a smoother viewing experience. Images will load as visitors scroll down the screen, instead of all at once.</p>
-								<?php 
-									$wp_version = (float) get_bloginfo( 'version' );
-									if (5.5 <= $wp_version):
-								?>
+								<?php
+  $wp_version = (float)get_bloginfo('version');
+  if (5.5 <= $wp_version):
+?>
 									<p>With the release of Version 5.5 of Wordpress Core, <a href="https://wordpress.org/support/wordpress-version/version-5-5/#speed" target="_blank">Lazy-Loading of images</a> has been introduced as a core feature and is enabled by default.</p>
 
-									<?php if ( !is_wp_error( $api ) ): $main_plugin_file = get_plugin_file($plugin);?>
+									<?php if (!is_wp_error($api)):
+      $main_plugin_file = get_plugin_file($plugin); ?>
 										<?php if (check_file_extension($main_plugin_file)): ?>
 											<p>We have detected that you are using the <a href="https://wordpress.org/plugins/wp-lazy-loading" target="_blank">Lazy Loading Feature Plugin</a>, you can proceed uninstall it, since it is no longer required.</p>
-										   <?php endif; ?>
-										<?php endif; ?>
-								<?php else: ?>
+										   <?php
+      endif; ?>
+										<?php
+    endif; ?>
+								<?php
+  else: ?>
 									<p>For lazy loading, we recommend the <a href="https://wordpress.org/plugins/wp-lazy-loading" target="_blank">Lazy Loading Feature Plugin</a> developed by the WordPress Core Team. This feature has been built into WordPress core since version 5.5 (<a href="https://wordpress.org/support/wordpress-version/version-5-5/#speed" target="_blank">Read More</a>). </p>
 
-									<?php if ( !is_wp_error( $api ) ): $main_plugin_file = get_plugin_file($plugin);?>
+									<?php if (!is_wp_error($api)):
+      $main_plugin_file = get_plugin_file($plugin); ?>
 									<div class="plugin">
 									  <div class="plugin-wrap">
 										  <img src="<?php echo $api->icons['default']; ?>" alt="">
@@ -194,23 +197,27 @@ function imagekit_render_imagekit_setting_page() {
 									   </div>
 									   <ul class="activation-row">
 									   <?php if (check_file_extension($main_plugin_file)): ?>
-											<?php if(is_plugin_active($main_plugin_file)): ?>
+											<?php if (is_plugin_active($main_plugin_file)): ?>
 											   <li>
 												   <a class="button disabled">Activated</a>
 											   </li>
-										   <?php else: ?>
+										   <?php
+        else: ?>
 												<li>
-												   <a class="activate button button-primary" href="plugins.php?action=activate&amp;plugin=<?php echo $main_plugin_file ?>&amp;_wpnonce=<?php echo wp_create_nonce('activate-plugin_'. $main_plugin_file) ?>" target="_parent">Activate Plugin</a>
+												   <a class="activate button button-primary" href="plugins.php?action=activate&amp;plugin=<?php echo $main_plugin_file ?>&amp;_wpnonce=<?php echo wp_create_nonce('activate-plugin_' . $main_plugin_file) ?>" target="_parent">Activate Plugin</a>
 											   </li>
-										   <?php endif; ?>
-									   <?php else: ?>
+										   <?php
+        endif; ?>
+									   <?php
+      else: ?>
 									   <li>
 										  <a class="install button"
-											href="<?php echo get_admin_url(); ?>/update.php?action=install-plugin&amp;plugin=<?php echo $api->slug; ?>&amp;_wpnonce=<?php echo wp_create_nonce('install-plugin_'. $api->slug) ?>">
+											href="<?php echo get_admin_url(); ?>/update.php?action=install-plugin&amp;plugin=<?php echo $api->slug; ?>&amp;_wpnonce=<?php echo wp_create_nonce('install-plugin_' . $api->slug) ?>">
 											Install Now
 										  </a>
 									   </li>
-									   <?php endif; ?>
+									   <?php
+      endif; ?>
 									   <li>
 										  <a href="https://wordpress.org/plugins/<?php echo $api->slug; ?>/" target="_blank">
 											 <?php _e('More Details', 'imagekit_domain'); ?>
@@ -218,8 +225,10 @@ function imagekit_render_imagekit_setting_page() {
 									   </li>
 									</ul>
 								   </div>
-									<?php endif; ?>
-								<?php endif; ?>
+									<?php
+    endif; ?>
+								<?php
+  endif; ?>
 							 </fieldset>
 						  </div>
 						</div>
@@ -240,7 +249,7 @@ function imagekit_render_imagekit_setting_page() {
                 
             </div>
 			<div class="ik-footer">
-				<?php $plugin_data = get_plugin_data( IK_PLUGIN_ENTRYPOINT );?>
+				<?php $plugin_data = get_plugin_data(IK_PLUGIN_ENTRYPOINT); ?>
 			    <ul class="ik-footer__links">
 				    <li class="ik-footer__link-item"><a href="https://imagekit.io/" target="_blank" rel="noopener noreferrer" class="ik-footer__link"><?php echo $plugin_data['Name'] ?> version <?php echo $plugin_data['Version'] ?></a></li>
 				</ul>
@@ -250,20 +259,20 @@ function imagekit_render_imagekit_setting_page() {
    </div>
 </div>
 <?php
-	echo ob_get_clean();
+  echo ob_get_clean();
 }
 
 function imagekit_add_setting_link() {
-	add_options_page('ImageKit settings', 'ImageKit settings', 'manage_options', 'imagekit-setting', 'imagekit_render_imagekit_setting_page');
+  add_options_page('ImageKit settings', 'ImageKit settings', 'manage_options', 'imagekit-setting', 'imagekit_render_imagekit_setting_page');
 }
 add_action('admin_menu', 'imagekit_add_setting_link');
 
 function imagekit_register_settings() {
-	add_filter('admin_body_class', function($classes) {
-		$classes .= ' ' . 'imagekit-pagestyles ';
-		return $classes;
-	});
-	register_setting('imagekit_settings_group', 'imagekit_settings');
+  add_filter('admin_body_class', function ($classes) {
+    $classes .= ' ' . 'imagekit-pagestyles ';
+    return $classes;
+  });
+  register_setting('imagekit_settings_group', 'imagekit_settings');
 }
 
 add_action('admin_init', 'imagekit_register_settings');
